807b8ff69d2c1797bdaf8581299261a257633433
riteshyadav-at-440464815922 <riteshyadav-at-440464815922>
2024-01-31 13:18:10 +0530

    navigation button

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,11 +1,17 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:foodly_flutter/constants/constants.dart';
+import 'package:get/get.dart';
+
+import '../controller/tabController.dart';
 
 class EntryPiont extends StatelessWidget {
   const EntryPiont({super.key});
 
   @override
   Widget build(BuildContext context) {
+     final  controller = Get.put(TabIndexController());
     return Scaffold(
       body: Stack(
         children: [
@@ -15,24 +21,31 @@ class EntryPiont extends StatelessWidget {
             height: hieght,
           ),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: Theme(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
                 data: Theme.of(context).copyWith(canvasColor: kPrimary),
                 child: BottomNavigationBar(
-                  showSelectedLabels: false,
-                  unselectedIconTheme:const IconThemeData(color: Colors.black38),
-                  items:const [
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.home), label: "home"),
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.search), label: "search"),
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.category), label: "category"),
-                    BottomNavigationBarItem(
-                        icon: Icon(Icons.account_circle_rounded), label: "profile"),
-                  ],
-                )),
-          )
+                      onTap: (value) {
+                         controller.settabIndex = value;
+                         print(controller.tabIndex);
+                      },
+                      showSelectedLabels: false,
+                      unselectedIconTheme:
+                          const IconThemeData(color: Colors.black38),
+                      selectedIconTheme: const IconThemeData(color: kSecondary),
+                      items: const [
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.home), label: "home"),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.search), label: "search"),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.category), label: "category"),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.account_circle_rounded),
+                            label: "profile"),
+                      ],
+                    ),
+              ))
         ],
       ),
     );
